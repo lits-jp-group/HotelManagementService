080c9877e02b1e2b3527855f2152284b4b2aa4bc
pashka555 <pprotasenya@gmail.com>
2019-06-30 10:13:05 +0300

    Staff member DTO.

--- src/main/java/lits/jp/hotel/management/dtos/StaffMemberDTO.java
+++ src/main/java/lits/jp/hotel/management/dtos/StaffMemberDTO.java
@@ -1,4 +1,17 @@
 package lits.jp.hotel.management.dtos;
 
+import lits.jp.hotel.management.models.Bookings;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class StaffMemberDTO {
+
+    public String firstName;
+
+    public String lastName;
+
+    public List<BookingsDTO> bookingsHandled;
+
 }
