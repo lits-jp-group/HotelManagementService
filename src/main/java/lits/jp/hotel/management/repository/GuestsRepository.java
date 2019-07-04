5af9aad5a03c31d2ed25474d9169b5d5b7d90a34
pashka555 <pprotasenya@gmail.com>
2019-07-04 15:42:54 +0300

    created GuestsRepository

---
+++ src/main/java/lits/jp/hotel/management/repository/GuestsRepository.java
@@ -0,0 +1,14 @@
+package lits.jp.hotel.management.repository;
+
+import lits.jp.hotel.management.models.Guests;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GuestsRepository extends CrudRepository<Guests, Integer> {
+
+    Iterable<Guests> findByFirstName(String firstName);
+
+    Iterable<Guests> findByLastName(String lastName);
+
+    Iterable<Guests> findByEmail (String email);
+
+}
