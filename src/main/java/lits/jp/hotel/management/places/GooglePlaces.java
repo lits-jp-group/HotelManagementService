fc7cac8e36db911e85f4428f9d1de71f1bb16b9c
Serhiy Perepelyuk <3774217@gmail.com>
2019-07-21 20:25:48 +0300

    repo tests runs ok. added couple rooms and couple guests for internal tests. third API -(google places) - works. available G.Places types: bank, bar, beauty_salon, cafe, church, food, university, zoo etc.

--- src/main/java/lits/jp/hotel/management/places/GooglePlaces.java
+++ src/main/java/lits/jp/hotel/management/places/GooglePlaces.java
@@ -1,21 +1,10 @@
 package lits.jp.hotel.management.places;
 
-// experinemt by loco
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.FileWriter;
 
 public class GooglePlaces {
 
@@ -26,31 +15,17 @@ public class GooglePlaces {
 
     // ARRAYS
     public static final String ARRAY_RESULTS = "results"; // Array for results
-    public static final String ARRAY_TYPES = "types"; // Types of place
 
     // DOUBLES
     public static final String DOUBLE_LATITUDE = "lat"; // Latitude of place
     public static final String DOUBLE_LONGITUDE = "lng"; // Longitude of place
-    public static final String DOUBLE_RATING = "rating"; // The "rating" of the place
 
     // OBJECTS
     public static final String OBJECT_GEOMETRY = "geometry"; // Geographic information
     public static final String OBJECT_LOCATION = "location"; // Contains latitude and longitude coordinates
 
-
     // STRINGS
-    public static final String STRING_ID = "id"; // The unique, stable, identifier for this place
     public static final String STRING_NAME = "name"; // The name of the place
-
-    //TYPES
-    public static final String TYPE_BANK = "bank";
-    public static final String TYPE_BAR = "bar";
-    public static final String TYPE_BEAUTY_SALON = "beauty_salon";
-    public static final String TYPE_CAFE = "cafe";
-    public static final String TYPE_CHURCH = "church";
-    public static final String TYPE_FOOD = "food";
-    public static final String TYPE_UNIVERSITY = "university";
-    public static final String TYPE_ZOO = "zoo";
 
     private String apiKey = "AIzaSyD-PwiwfuC70H1ZFfqPkX3Wmdm6NCbxxXc"; // ключ працює лише 10 чи 12 запитів в добу. тому зараз поставив "заглушку".
 
@@ -103,9 +78,11 @@ public class GooglePlaces {
 
 
 //
-// ------------------------ BEGIN READ FROM RESPOSEMOCK --------------
+// ------------------------ BEGIN READ FROM RESPONSEMOCK --------------
         ResponseMock rs = new ResponseMock();
-        String contents = rs.giveResponse2();
+        String contents = rs.giveMockedResponse();
+// ------------------------ END READ FROM RESPONSEMOCK --------------
+
 
         response = contents;
 
--- src/main/java/lits/jp/hotel/management/places/ResponseMock.java
+++ src/main/java/lits/jp/hotel/management/places/ResponseMock.java
@@ -8,20 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ResponseMock {
-    public String giveResponse (String response){
 
-        String contents = null;
-        try {
-            contents = new String(Files.readAllBytes(Paths.get("d://response.txt")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("response " + contents);
-        response = contents;
-        return response;
-    }
-
-    public String giveResponse2 () throws IOException {
+    public String giveMockedResponse() throws IOException {
         FileInputStream fis =  new FileInputStream("d://response.txt");
         BufferedReader r = new BufferedReader(new InputStreamReader(fis, "Cp1251"));
         String myString = r.readLine();
